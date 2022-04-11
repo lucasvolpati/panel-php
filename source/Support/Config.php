66041520f7dc2577197d6037ced60405fbef0bcf
Lucas Alcantara Rodrigues Volpati <lcs.rodrigues16@hotmail.com>
2022-04-11 11:46:22 -0300

    generate php files

---
+++ source/Support/Config.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * DATABASE
+ */
+define('CONF_DB_HOST', 'localhost');
+define('CONF_DB_USER', 'lucas');
+define('CONF_DB_PASS', 'Luk1805@');
+define('CONF_DB_NAME', 'panel');
+
+
+/**
+ * PASSWORD
+ */
+define("CONF_PASSWD_MIN_LEN", 8);
+define("CONF_PASSWD_MAX_LEN", 40);
+define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
+define("CONF_PASSWD_OPTION", ["cost" => 10]);
+
+
+/**
+ * MESSAGE
+ */
+define("CONF_MESSAGE_CLASS", "trigger");
+define("CONF_MESSAGE_INFO", "info");
+define("CONF_MESSAGE_SUCCESS", "success");
+define("CONF_MESSAGE_WARNING", "warning");
+define("CONF_MESSAGE_ERROR", "error");
