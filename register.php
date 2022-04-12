4fe4628664f38b92d59bef98598b0b3a77588573
Lucas Alcantara Rodrigues Volpati <lcs.rodrigues16@hotmail.com>
2022-04-12 11:10:27 -0300

    register

--- register.php
+++ register.php
@@ -8,9 +8,9 @@ use Source\Core\Message;
 
 $con = Connect::getInstance()->prepare("INSERT INTO users (name,email,password) VALUES (:name, :email, :pass)");
 
-$name = 'Lucas';
-$email = 'lucas@peachbrasil.com.br';
-$pass = password_hash("marcelly", CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
+// $name = 'Lucas';
+// $email = 'lucas@peachbrasil.com.br';
+// $pass = password_hash("", CONF_PASSWD_ALGO, CONF_PASSWD_OPTION);
 
 $con->bindValue(":name", $name);
 $con->bindValue(":email", $email);
